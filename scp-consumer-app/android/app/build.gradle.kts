7dac96dafa66a63ae593f14bad7de185843a96e5
akydyrbay <akdrbb@gmail.com>
2025-11-03 02:31:55 +0500

    fix: gradle

--- scp-consumer-app/android/app/build.gradle.kts
+++ scp-consumer-app/android/app/build.gradle.kts
@@ -23,6 +23,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -65,6 +66,10 @@ android {
             isMinifyEnabled = false
         }
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 flutter {
